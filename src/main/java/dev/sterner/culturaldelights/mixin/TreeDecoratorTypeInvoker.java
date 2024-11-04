15c2f284184a5dbeeea4af72028ad56855b24478
emirsassan <endwse@gmail.com>
2024-11-04 22:57:35 +0300

    minor fix for TreeDecoratorTypeInvoker

--- src/main/java/dev/sterner/culturaldelights/mixin/TreeDecoratorTypeInvoker.java
+++ src/main/java/dev/sterner/culturaldelights/mixin/TreeDecoratorTypeInvoker.java
@@ -1,6 +1,7 @@
 package dev.sterner.culturaldelights.mixin;
 
 import com.mojang.serialization.Codec;
+import com.mojang.serialization.MapCodec;
 import net.minecraft.world.gen.treedecorator.TreeDecorator;
 import net.minecraft.world.gen.treedecorator.TreeDecoratorType;
 import org.spongepowered.asm.mixin.Mixin;
@@ -9,8 +10,8 @@ import org.spongepowered.asm.mixin.gen.Invoker;
 @Mixin(TreeDecoratorType.class)
 public interface TreeDecoratorTypeInvoker {
 
-    @Invoker("<init>")
-    static <P extends TreeDecorator> TreeDecoratorType<P> register(String id, Codec<P> codec) {
+    @Invoker("register")
+    static <P extends TreeDecorator> TreeDecoratorType<P> register(String id, MapCodec<P> codec) {
         throw new AssertionError();
     }
 }
